32f024f352bef0f771f2b22e2a7a15f59f03b2b9
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-06-13 01:53:40 +0800

    Update billplzWoo.php
    Version update

--- billplz-for-woocommerce/billplzWoo.php
+++ billplz-for-woocommerce/billplzWoo.php
@@ -5,7 +5,7 @@
  * Description: Plugin Billplz untuk WooCommerce. Sokong projek Payment Gateway percuma untuk semua Online Shopping Cart. Sumbangan boleh dilakukan disini. <a href="https://www.billplz.com/form/sw2co7ig8" target="_blank">Donate Now</a>.
  * Author: Wanzul Hosting Enterprise
  * Author URI: http://www.wanzul-hosting.com/
- * Version: 3.5
+ * Version: 3.6
  * License: GPLv3
  * Text Domain: wcbillplz
  * Domain Path: /languages/
