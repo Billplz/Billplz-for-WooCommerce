9f102b9420dbd7b1b67620728640e6ff0ed1320b
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2019-08-16 10:39:02 +0800

    Update billplzWoo.php

--- billplzWoo.php
+++ billplzWoo.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Billplz for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/billplz-for-woocommerce/
- * Description: Billplz Payment Gateway | <a href="https://www.billplz.com/enterprise/signup" target="_blank">Sign up Now</a>.
+ * Description: Billplz. Fair payment platform. | <a href="https://www.billplz.com/enterprise/signup" target="_blank">Sign up Now</a>.
  * Author: Billplz Sdn. Bhd.
  * Author URI: http://github.com/billplz/billplz-for-woocommerce
  * Version: 3.21.9
