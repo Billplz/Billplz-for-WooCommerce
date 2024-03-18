e3db0a16e50737532b36b7146bf1380baf95fc18
Yied Pozi <yiedpozi@gmail.com>
2024-03-18 11:09:13 +0800

    update plugin version and changelog

--- billplzWoo.php
+++ billplzWoo.php
@@ -6,7 +6,7 @@
  * Description: Billplz. Fair payment platform.
  * Author: Billplz Sdn Bhd
  * Author URI: http://github.com/billplz/billplz-for-woocommerce
- * Version: 3.28.3
+ * Version: 3.28.4
  * Requires PHP: 7.0
  * Requires at least: 4.6
  * Requires Plugins: woocommerce
